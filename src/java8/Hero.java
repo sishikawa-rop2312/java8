f7a35525d11ab29613b4c1f6e944cb8d41bf8b5e
sishikawa-rop2312 <tukuyomi610315@gmail.com>
2024-01-10 20:23:00 +0900

    コード8-13まで着手

--- src/java8/Hero.java
+++ src/java8/Hero.java
@@ -8,4 +8,22 @@ public class Hero {
 		this.hp = 100;
 		System.out.printf("%sは、眠って回復した！\n", this.name);
 	}
+	
+	public void sit(int sec) {
+		this.hp += sec;
+		System.out.printf("%sは、%d秒座った！\n", this.name, sec);
+		System.out.printf("HPが%dポイント回復した\n", sec);
+	}
+	
+	public void slip() {
+		this.hp -= 5;
+		System.out.printf("%sは転んだ！\n", this.name);
+		System.out.printf("5のダメージ！\n");
+	}
+	
+	public void run() {
+		System.out.printf("%sは、逃げだした！\n", this.name);
+		System.out.printf("GAMEOVER\n");
+		System.out.printf("最終HPは%dでした！\n", this.hp);
+	}
 }
--- src/java8/Main.java
+++ src/java8/Main.java
@@ -1,10 +1,19 @@
 package java8;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		// 1. 勇者を生成
+		Hero h = new Hero();
+		
+		// 2. フィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.printf("勇者%sを生み出しました！\n", h.name);
+		
+		// 3. 勇者のメソッドを呼び出していく
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
-
 }
